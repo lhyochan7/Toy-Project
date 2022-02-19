d51456e8d6aa07d8775011feb18603d489911eb5
hyochan <lhyochan7@gmail.com>
2022-02-19 19:11:42 +0900

    updated

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -34,6 +34,7 @@ public class ItemRepository {
     public void update(Long itemId, Item updateParam) {
         Item findItem = findById(itemId);
         findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
     }
 
