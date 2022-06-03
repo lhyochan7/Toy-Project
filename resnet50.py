5b9c10014393bf8f78a56e0eb2474acf5c3943e2
hyochan <lhyochan7@gmail.com>
2022-06-03 22:16:59 +0900

    added feature extractor for resnet50 on 2022/06/03

--- resnet50.py
+++ resnet50.py
@@ -1,6 +1,23 @@
+import torch
+import torch.nn as nn
 import torchvision.models as models
 
-if __name__ == "__main__":
-    resnet50 = models.resnet50(pretrained=True)
+class ResNet50Bottom(nn.Module):
+    def __init__(self, original_model):
+        super(ResNet50Bottom, self).__init__()
+        self.features = nn.Sequential(*list(original_model.children())[:-2])
+        
+    def forward(self, x):
+        x = self.features(x)
+        return x
 
-    print(resnet50)
+
+if __name__ == "__main__":
+
+    inputs = torch.rand(1, 3, 224, 224)
+    res50_model = models.resnet50(pretrained=True)
+    res50_conv2 = ResNet50Bottom(res50_model)
+
+    outputs = res50_conv2(inputs)
+    print(outputs.data.shape)  # => torch.Size([4, 2048, 7, 7])
+
